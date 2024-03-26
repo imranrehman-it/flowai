dfceb13212b05aeb65df6d55e09c7d7206cc1a20
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-03-26 15:09:47 -0400

    github auth page

--- gpt-pro/src/pages/login.tsx
+++ gpt-pro/src/pages/login.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { signIn } from "next-auth/react";
+
 
 export default function Login() {
   return (
@@ -27,7 +29,7 @@ export default function Login() {
                                     <svg className="w-5 h-5 mr-2" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512"><path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C299.9 93.7 276.8 88 248 88c-94.3 0-172 76.5-172 172s77.7 172 172 172c74.4 0 128.5-49.9 149.3-116H248v-92h240Z"></path></svg>
                                     Google
                                 </button>
-                                <button type="button" className="flex items-center justify-center w-full px-4 py-2.5 bg-white border border-gray-300 rounded-lg text-sm text-gray-900 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-primary-600 ml-2">
+                                <button onClick={()=>signIn('github')} type="button" className="flex items-center justify-center w-full px-4 py-2.5 bg-white border border-gray-300 rounded-lg text-sm text-gray-900 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-gray-800 dark:border-gray-600 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-primary-600 ml-2">
                                     <svg className="w-5 h-5 mr-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
                                     GitHub
                                 </button>
