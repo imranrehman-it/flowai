702d8a8057ee47c2dd83c526df5d5b6eba679a60
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-03-28 06:35:42 -0400

    added icons and better chat rendering

--- gpt-pro/src/app/components/Home/ChatBubble.tsx
+++ gpt-pro/src/app/components/Home/ChatBubble.tsx
@@ -1,24 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import { MarkdownRenderer } from '../markdown/Markdown';
+import { Image } from 'openai/resources/images.mjs';
+import { useSession } from 'next-auth/react';
+
 
 
 export const ChatBubble = ({ text, position }) => {
-  const rightStyles = 'flex justify-end mb-2';
-  const leftStyles = 'flex justify-start mb-2';
+
+  const {data: session, status} = useSession()
+  const [icon, setIcon] = useState<any>()
 
   const [style, setStyle] = useState('flex justify-end mb-2');
 
   useEffect(() => {
+    
     if (position === 'left') {
-      setStyle(leftStyles);
+      setIcon("https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg")
     } else {
-      setStyle(rightStyles);
+      setIcon(session?.user?.image)
     }
   }, [position]);
 
   return (
-    <div className={style}>
-      <div className="bg-gray-800 p-3 rounded-md font-bold text-white" style={{ maxWidth: '50%' }}>
+    <div className='flex mb-2'>
+      <div className="bg-gray-800 p-3 rounded-md text-white items-start" style={{ maxWidth: '70%' }} >
+        <img src={icon} alt="logo" width={16} height={16} className="mr-2" />
         <MarkdownRenderer>{text}</MarkdownRenderer>
       </div>
     </div>
