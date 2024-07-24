019ad969cf231f316266e44310594a6cf4a176a5
imranrehman-it <imran.rehman@mail.utoronto.ca>
2024-07-24 16:35:10 -0400

    removed expty prompts from prompt bar

--- gpt-pro/src/app/components/Home/PromptBar.tsx
+++ gpt-pro/src/app/components/Home/PromptBar.tsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import { useChat } from '../../../context/ChatContext'
-interface Chat {
-    id: string
-    title: string
-    messages: Message[]
-}
 
-interface Message {
-    role: string
-    content: string
-}
+
 
 export const PromptBar = ({scrollToChat}:{scrollToChat: (id: string) => void}) => {
     const {currentChat} = useChat()
 
   return (
      <aside className="bg-gray-900 text-white w-64 p-4 m-2 hidden lg:block rounded-lg overflow-scroll">
-        {currentChat?.messages.map((message: {role: string, message: string}, index: number) => (
-            <div key={index} onClick={() => scrollToChat(index.toString())} className='flex border-b border-gray-700 p-2 hover:bg-gray-800 rounded-sm'>
+        {currentChat?.messages.map((message: {role: string, message: string}, index: number) => {
+            if(message.role.length === 0){
+                return
+            }
+            return(
+                <div key={index} onClick={() => scrollToChat(index.toString())} className='flex border-b border-gray-700 p-2 hover:bg-gray-800 rounded-sm'>
                <div className='max-h-36 overflow-hidden'>
                     <p className='text-sm font-semibold'>{message.role}</p>
                </div>
             </div>
-        ))}
+            )
+            
+    })}
     </aside>
   )
 }
